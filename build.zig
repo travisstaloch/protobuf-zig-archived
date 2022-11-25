7a90504a05bd84fb3f568a4dd4d27e6345f0327c
Travis Staloch <twostepted@gmail.com>
2022-11-24 22:32:25 -0800

    add 'log-level' build option

--- build.zig
+++ build.zig
@@ -4,6 +4,15 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
+    const log_level = b.option(
+        []const u8,
+        "log-level",
+        "The log level for the application. default .err",
+    ) orelse "err";
+
+    const build_options = b.addOptions();
+    build_options.addOption([]const u8, "log_level", log_level);
+
     const protocgen_echo = b.addExecutable("protoc-gen-zig", "src/protoc-gen-echo.zig");
     protocgen_echo.setTarget(target);
     protocgen_echo.setBuildMode(mode);
@@ -13,12 +22,14 @@ pub fn build(b: *std.build.Builder) void {
         .source = .{ .path = "src/decoding.zig" },
     };
     protocgen_echo.addPackage(decoding_pkg);
+    protocgen_echo.addOptions("build_options", build_options);
 
     const protoc_zig = b.addExecutable("protoc-zig", "src/main.zig");
     protoc_zig.setTarget(target);
     protoc_zig.setBuildMode(mode);
     protoc_zig.install();
     protoc_zig.addPackage(decoding_pkg);
+    protoc_zig.addOptions("build_options", build_options);
 
     const run_cmd = protoc_zig.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -33,6 +44,7 @@ pub fn build(b: *std.build.Builder) void {
     parsing_test.setBuildMode(mode);
     parsing_test.install();
     parsing_test.addPackage(decoding_pkg);
+    parsing_test.addOptions("build_options", build_options);
 
     const parsing_test_step = b.step("parsing-test", "Run a parsing test");
     const parsing_test_run_cmd = parsing_test.run();
--- src/util.zig
+++ src/util.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
-pub const log_level: std.log.Level = .err;
+const log_level_str = @import("build_options").log_level;
+pub const log_level: std.log.Level = std.meta.stringToEnum(std.log.Level, log_level_str) orelse
+    @compileError("invalid log-level '" ++ log_level_str ++ "'");
 
 pub fn todo(comptime fmt: []const u8, args: anytype) noreturn {
     std.debug.panic("TODO: " ++ fmt ++ "\n", args);
